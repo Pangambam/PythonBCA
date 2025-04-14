3cf3634702b2137c6fb1c8dee13b42fa563fe33a
Meikaappi <53959992+Pangambam@users.noreply.github.com>
2025-04-14 23:13:57 +0530

    Create TypeCasting.py

---
+++ TypeCasting.py
@@ -0,0 +1,19 @@
+# Integer to float
+num = 10
+print("Integer:", num)
+print("Float:", float(num))
+
+# Float to integer
+decimal = 9.87
+print("Float:", decimal)
+print("Integer:", int(decimal))
+
+# String to integer
+str_num = "100"
+print("String:", str_num)
+print("Integer:", int(str_num))
+
+# Integer to string
+age = 25
+print("Integer:", age)
+print("String:", str(age))
