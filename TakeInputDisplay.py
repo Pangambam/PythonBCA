e086a8d3f1a5d5df6d2de4c7cc38cedce9d7f3a0
Meikaappi <53959992+Pangambam@users.noreply.github.com>
2025-04-14 23:13:33 +0530

    Create TakeInputDisplay.py

---
+++ TakeInputDisplay.py
@@ -0,0 +1,9 @@
+# Taking input from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+marks = float(input("Enter your marks: "))
+
+# Displaying output
+print("Hello", name + "!")
+print("You are", age, "years old.")
+print("You scored", marks, "marks.")
