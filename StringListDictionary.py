9e14c198b9b77b3918cb32dcf196cf42182ca2d4
Meikaappi <53959992+Pangambam@users.noreply.github.com>
2025-04-14 23:21:58 +0530

    Create StringListDictionary.py

---
+++ StringListDictionary.py
@@ -0,0 +1,14 @@
+# String functions
+s = "Hello World"
+print("Length of string:", len(s))
+print("Is string alphanumeric?", s.isalnum())
+
+# List functions
+l = [1, 2, 3, 4]
+print("Max value in list:", max(l))
+print("Sum of list elements:", sum(l))
+
+# Dictionary functions
+d = {"a": 1, "b": 2, "c": 3}
+print("Keys:", d.keys())
+print("Values:", d.values())
