4e98c8c3e4b26b606e07332dfb00b57a6639e40e
Meikaappi <53959992+Pangambam@users.noreply.github.com>
2025-04-14 23:20:06 +0530

    Create TupleAccessing.py

---
+++ TupleAccessing.py
@@ -0,0 +1,7 @@
+# Creating a tuple
+my_tuple = ("apple", "banana", "cherry")
+
+# Accessing elements
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-1])
+print("All elements:", my_tuple)
